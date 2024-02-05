c402e85a6251a3fdc696e23ffc02cb83419a012a
Flogas <florentgasseau@hotmail.com>
2024-02-05 22:27:11 +0100

    Coordinates conversion with DotSpatial - POC

--- Forms/FrmDebug.Designer.cs
+++ Forms/FrmDebug.Designer.cs
@@ -38,12 +38,30 @@
 			LbKtas = new Label();
 			advancedDataGridView1 = new Zuby.ADGV.AdvancedDataGridView();
 			groupBox1 = new GroupBox();
+			GbCoordinates = new GroupBox();
+			CbMap = new ComboBox();
+			label2 = new Label();
+			BtConvetGeoToDcs = new Button();
+			NudLong = new NumericUpDown();
+			label3 = new Label();
+			NudLat = new NumericUpDown();
+			label4 = new Label();
+			NudY = new NumericUpDown();
+			label1 = new Label();
+			NudX = new NumericUpDown();
+			LbX = new Label();
+			BtConvertDcsToGeo = new Button();
 			GbSpeeds.SuspendLayout();
 			((System.ComponentModel.ISupportInitialize)NudAltitude).BeginInit();
 			((System.ComponentModel.ISupportInitialize)NudKias).BeginInit();
 			((System.ComponentModel.ISupportInitialize)NudKtas).BeginInit();
 			((System.ComponentModel.ISupportInitialize)advancedDataGridView1).BeginInit();
 			groupBox1.SuspendLayout();
+			GbCoordinates.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)NudLong).BeginInit();
+			((System.ComponentModel.ISupportInitialize)NudLat).BeginInit();
+			((System.ComponentModel.ISupportInitialize)NudY).BeginInit();
+			((System.ComponentModel.ISupportInitialize)NudX).BeginInit();
 			SuspendLayout();
 			// 
 			// GbSpeeds
@@ -57,7 +75,7 @@
 			GbSpeeds.Controls.Add(LbKtas);
 			GbSpeeds.Location = new Point(12, 12);
 			GbSpeeds.Name = "GbSpeeds";
-			GbSpeeds.Size = new Size(776, 139);
+			GbSpeeds.Size = new Size(776, 88);
 			GbSpeeds.TabIndex = 0;
 			GbSpeeds.TabStop = false;
 			GbSpeeds.Text = "Speed conversion";
@@ -140,18 +158,152 @@
 			// groupBox1
 			// 
 			groupBox1.Controls.Add(advancedDataGridView1);
-			groupBox1.Location = new Point(12, 157);
+			groupBox1.Location = new Point(12, 254);
 			groupBox1.Name = "groupBox1";
-			groupBox1.Size = new Size(776, 678);
+			groupBox1.Size = new Size(776, 581);
 			groupBox1.TabIndex = 2;
 			groupBox1.TabStop = false;
 			groupBox1.Text = "groupBox1";
+			// 
+			// GbCoordinates
+			// 
+			GbCoordinates.Controls.Add(CbMap);
+			GbCoordinates.Controls.Add(label2);
+			GbCoordinates.Controls.Add(BtConvetGeoToDcs);
+			GbCoordinates.Controls.Add(NudLong);
+			GbCoordinates.Controls.Add(label3);
+			GbCoordinates.Controls.Add(NudLat);
+			GbCoordinates.Controls.Add(label4);
+			GbCoordinates.Controls.Add(NudY);
+			GbCoordinates.Controls.Add(label1);
+			GbCoordinates.Controls.Add(NudX);
+			GbCoordinates.Controls.Add(LbX);
+			GbCoordinates.Controls.Add(BtConvertDcsToGeo);
+			GbCoordinates.Location = new Point(12, 106);
+			GbCoordinates.Name = "GbCoordinates";
+			GbCoordinates.Size = new Size(776, 122);
+			GbCoordinates.TabIndex = 3;
+			GbCoordinates.TabStop = false;
+			GbCoordinates.Text = "Coordinates conversion";
+			// 
+			// CbMap
+			// 
+			CbMap.FormattingEnabled = true;
+			CbMap.Location = new Point(86, 38);
+			CbMap.Name = "CbMap";
+			CbMap.Size = new Size(121, 23);
+			CbMap.TabIndex = 17;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(25, 39);
+			label2.Name = "label2";
+			label2.Size = new Size(31, 15);
+			label2.TabIndex = 16;
+			label2.Text = "Map";
+			// 
+			// BtConvetGeoToDcs
+			// 
+			BtConvetGeoToDcs.Location = new Point(438, 72);
+			BtConvetGeoToDcs.Name = "BtConvetGeoToDcs";
+			BtConvetGeoToDcs.Size = new Size(75, 23);
+			BtConvetGeoToDcs.TabIndex = 15;
+			BtConvetGeoToDcs.Text = "< Convert";
+			BtConvetGeoToDcs.UseVisualStyleBackColor = true;
+			// 
+			// NudLong
+			// 
+			NudLong.DecimalPlaces = 8;
+			NudLong.Location = new Point(624, 68);
+			NudLong.Minimum = new decimal(new int[] { 100, 0, 0, int.MinValue });
+			NudLong.Name = "NudLong";
+			NudLong.Size = new Size(120, 23);
+			NudLong.TabIndex = 14;
+			NudLong.ThousandsSeparator = true;
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(580, 70);
+			label3.Name = "label3";
+			label3.Size = new Size(34, 15);
+			label3.TabIndex = 13;
+			label3.Text = "Long";
+			// 
+			// NudLat
+			// 
+			NudLat.DecimalPlaces = 8;
+			NudLat.Location = new Point(624, 39);
+			NudLat.Minimum = new decimal(new int[] { 100, 0, 0, int.MinValue });
+			NudLat.Name = "NudLat";
+			NudLat.Size = new Size(120, 23);
+			NudLat.TabIndex = 12;
+			NudLat.ThousandsSeparator = true;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(580, 43);
+			label4.Name = "label4";
+			label4.Size = new Size(23, 15);
+			label4.TabIndex = 11;
+			label4.Text = "Lat";
+			// 
+			// NudY
+			// 
+			NudY.Location = new Point(297, 68);
+			NudY.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
+			NudY.Minimum = new decimal(new int[] { 1000000, 0, 0, int.MinValue });
+			NudY.Name = "NudY";
+			NudY.Size = new Size(120, 23);
+			NudY.TabIndex = 10;
+			NudY.ThousandsSeparator = true;
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Location = new Point(262, 76);
+			label1.Name = "label1";
+			label1.Size = new Size(14, 15);
+			label1.TabIndex = 9;
+			label1.Text = "Y";
+			// 
+			// NudX
+			// 
+			NudX.Location = new Point(297, 39);
+			NudX.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
+			NudX.Minimum = new decimal(new int[] { 1000000, 0, 0, int.MinValue });
+			NudX.Name = "NudX";
+			NudX.Size = new Size(120, 23);
+			NudX.TabIndex = 8;
+			NudX.ThousandsSeparator = true;
+			// 
+			// LbX
+			// 
+			LbX.AutoSize = true;
+			LbX.Location = new Point(262, 41);
+			LbX.Name = "LbX";
+			LbX.Size = new Size(14, 15);
+			LbX.TabIndex = 7;
+			LbX.Text = "X";
+			// 
+			// BtConvertDcsToGeo
+			// 
+			BtConvertDcsToGeo.Location = new Point(438, 43);
+			BtConvertDcsToGeo.Name = "BtConvertDcsToGeo";
+			BtConvertDcsToGeo.Size = new Size(75, 23);
+			BtConvertDcsToGeo.TabIndex = 4;
+			BtConvertDcsToGeo.Text = "Convert >";
+			BtConvertDcsToGeo.UseVisualStyleBackColor = true;
+			BtConvertDcsToGeo.Click += BtConvertDcsToGeo_Click;
 			// 
 			// FrmDebug
 			// 
 			AutoScaleDimensions = new SizeF(7F, 15F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(800, 847);
+			Controls.Add(GbCoordinates);
 			Controls.Add(groupBox1);
 			Controls.Add(GbSpeeds);
 			Name = "FrmDebug";
@@ -165,6 +317,12 @@
 			((System.ComponentModel.ISupportInitialize)NudKtas).EndInit();
 			((System.ComponentModel.ISupportInitialize)advancedDataGridView1).EndInit();
 			groupBox1.ResumeLayout(false);
+			GbCoordinates.ResumeLayout(false);
+			GbCoordinates.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)NudLong).EndInit();
+			((System.ComponentModel.ISupportInitialize)NudLat).EndInit();
+			((System.ComponentModel.ISupportInitialize)NudY).EndInit();
+			((System.ComponentModel.ISupportInitialize)NudX).EndInit();
 			ResumeLayout(false);
 		}
 
@@ -180,5 +338,18 @@
 		private Label LbAltitude;
 		private Zuby.ADGV.AdvancedDataGridView advancedDataGridView1;
 		private GroupBox groupBox1;
+		private GroupBox GbCoordinates;
+		private Button BtConvertDcsToGeo;
+		private NumericUpDown NudLong;
+		private Label label3;
+		private NumericUpDown NudLat;
+		private Label label4;
+		private NumericUpDown NudY;
+		private Label label1;
+		private NumericUpDown NudX;
+		private Label LbX;
+		private Button BtConvetGeoToDcs;
+		private ComboBox CbMap;
+		private Label label2;
 	}
 }
--- Forms/FrmDebug.cs
+++ Forms/FrmDebug.cs
@@ -1,6 +1,5 @@
 ﻿using DcsBriefop.Tools;
 using static DcsBriefop.Tools.ToolsSpeeds;
-using UnitsNet;
 
 namespace DcsBriefop.Forms
 {
@@ -13,6 +12,7 @@ namespace DcsBriefop.Forms
 		public FrmDebug()
 		{
 			InitializeComponent();
+			CoordinatesInitialize();
 		}
 		#endregion
 
@@ -33,6 +33,46 @@ namespace DcsBriefop.Forms
 			//double dAltitudeMeters = UnitConverter.Convert(dAltitudeFeet, UnitsNet.Units.LengthUnit.Foot, UnitsNet.Units.LengthUnit.Meter);
 			ComputedSpeeds speeds = ToolsSpeeds.ConvertTrueAirSpeed(dKtas, dAltitudeMeters);
 			NudKias.Value = (decimal)speeds.KIAS;
+		}
+		#endregion
+
+		#region Coordinates
+		private DotSpatial.Projections.ProjectionInfo projWgs84 = DotSpatial.Projections.ProjectionInfo.FromProj4String("+proj=latlong +datum=WGS84 +no_defs");
+		private void CoordinatesInitialize()
+		{
+			CbMap.ValueMember = "Value";
+			CbMap.DisplayMember = "Key";
+			CbMap.DataSource = new Dictionary<string, string>()
+			{
+				{"Caucasus", "+proj=tmerc +lat_0=0 +lon_0=33 +k_0=0.9996 +x_0=-99517 +y_0=-4998115"},
+				{"Syria", "+proj=tmerc +lat_0=35.21917 +lon_0=35.9055 +k_0=0.9996 +x_0=0 +y_0=0"},
+				{"PersianGulf", "+proj=tmerc +lat_0=26.1718 +lon_0=56.2419 +k_0=0.9996 +x_0=0 +y_0=0"},
+				{"MarianaIslands", "+proj=tmerc +lat_0=35.21917 +lon_0=35.9055 +k_0=0.9996 +x_0=0 +y_0=0"},
+			}.ToList();
+		}
+
+		private Tuple<double, double> Reproject(DotSpatial.Projections.ProjectionInfo src, DotSpatial.Projections.ProjectionInfo trg, Tuple<double, double> coordSrc)
+		{
+			double[] xy = { coordSrc.Item1 , coordSrc.Item2 };
+			double[] z = { 0 };
+
+			DotSpatial.Projections.Reproject.ReprojectPoints(xy, z, src, trg, 0, 1);
+
+			return new Tuple<double, double>(xy[0], xy[1]);
+		}
+
+		private void BtConvertDcsToGeo_Click(object sender, EventArgs e)
+		{
+			string sSrcStringProj = CbMap.SelectedValue?.ToString();
+			if (string.IsNullOrEmpty(sSrcStringProj))
+				return;
+
+			DotSpatial.Projections.ProjectionInfo src = DotSpatial.Projections.ProjectionInfo.FromProj4String(sSrcStringProj);
+
+			Tuple<double, double> res = Reproject(src, projWgs84, new Tuple<double, double>((double)NudX.Value, (double)NudY.Value));
+
+			NudLat.Value = (decimal)res.Item1;
+			NudLong.Value = (decimal)res.Item2;
 		}
 		#endregion
 	}
